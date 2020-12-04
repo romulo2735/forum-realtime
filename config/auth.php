a7b767e1d7119ce9c05e9f0e6d7562cc753599ce
Romulo Sousa <romulosousa27@hotmail.com>
2020-12-04 16:17:30 -0300

    alterando o namespace com o nome do projeto

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => ForumRealtime\User::class,
         ],
 
         // 'users' => [
