9b2ae65c2e9ee90c73c54cd441a2b75f2d702fe1
Romulo Sousa <romulosousa27@hotmail.com>
2020-12-04 16:40:39 -0300

    Controllers de Thread e Reply

---
+++ app/Http/Controllers/ReplyController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ForumRealtime\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReplyController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace ForumRealtime\Http\Controllers;
+
+use ForumRealtime\Models\Thread;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ThreadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Thread $thread
+     * @return Response
+     */
+    public function show(Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Thread $thread
+     * @return Response
+     */
+    public function edit(Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Thread $thread
+     * @return Response
+     */
+    public function update(Request $request, Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Thread $thread
+     * @return Response
+     */
+    public function destroy(Thread $thread)
+    {
+        //
+    }
+}
