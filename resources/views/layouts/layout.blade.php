4684ff8ed86437f926c5e986ef15ee572167177e
Romulo Sousa <romulosousa27@hotmail.com>
2020-12-10 22:38:03 -0300

    Definindo um Layouy base

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forum Realtime</title>
+</head>
+<body>
+
+@yield('content')
+
+<script src="{{ asset('js/manifest.js') }}"></script>
+<script src="{{ asset('js/vendor.js') }}"></script>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+<script src="{{ asset('js/threads.js') }}"></script>
+
+</body>
+</html>
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,10 +1,7 @@
-<meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.layout')
 
-<div id="app">
-    <example-component>Loading...</example-component>
-</div>
-
-<script src="{{ asset('js/manifest.js') }}"></script>
-<script src="{{ asset('js/vendor.js') }}"></script>
-<script src="{{ asset('js/bootstrap.js') }}"></script>
-<script src="{{ asset('js/threads.js') }}"></script>
+@section('content')
+    <div id="app">
+        <example-component>Loading...</example-component>
+    </div>
+@endsection
