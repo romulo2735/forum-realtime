7902edbe18e118125e998c81c614004a2ec464a5
Romulo Sousa <romulosousa27@hotmail.com>
2020-12-10 22:33:30 -0300

    Configurando o Laravel Mix, para compilação de componentes Vue

---
+++ public/js/boostrap.js
@@ -0,0 +1,18 @@
+webpackJsonp([2],[
+/* 0 */,
+/* 1 */,
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackMissingModule() { throw new Error("Cannot find module \"C:\\Users\\romul\\projetos\\php\\laravel\\forum-realtime\\resources\\assets\\js\\boostrap.js\""); }());
+module.exports = __webpack_require__(3);
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[2]);
---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,10 @@
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<div id="app">
+    <example-component>Loading...</example-component>
+</div>
+
+<script src="{{ asset('js/manifest.js') }}"></script>
+<script src="{{ asset('js/vendor.js') }}"></script>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+<script src="{{ asset('js/threads.js') }}"></script>
