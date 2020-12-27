da263affbb544f715969cbfb0568816d4486be8c
Romulo Sousa <romulosousa27@hotmail.com>
2020-12-26 22:24:08 -0300

    consertando o slot de scripts

--- resources/views/layouts/base/scripts.blade.php
+++ resources/views/layouts/base/scripts.blade.php
@@ -1,4 +1,4 @@
 <script src="{{ asset('js/manifest.js') }}"></script>
 <script src="{{ asset('js/vendor.js') }}"></script>
 <script src="{{ asset('js/bootstrap.js') }}"></script>
-{{ $script }}
+{{ $slot }}
